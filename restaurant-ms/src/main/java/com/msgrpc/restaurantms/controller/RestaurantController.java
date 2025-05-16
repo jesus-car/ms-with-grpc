a2b6bb121c015cb3fa56bc2670caebe071a8e6eb
jesus-car <jesus.garcia2611@hotmail.com>
2025-05-16 14:29:09 -0500

    add gateway

--- restaurant-ms/src/main/java/com/msgrpc/restaurantms/controller/RestaurantController.java
+++ restaurant-ms/src/main/java/com/msgrpc/restaurantms/controller/RestaurantController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
-@RequestMapping("/restaurants")
+@RequestMapping("/restaurant")
 @RequiredArgsConstructor
 public class RestaurantController {
 
